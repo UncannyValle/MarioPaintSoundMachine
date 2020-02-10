4e20ebabab223b80f13708d3ec849e9ce54150f4
uncannyvalle <jvalle0926@gmail.com>
2020-02-09 21:24:49 -0800

    passed test 3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, {useState} from 'react';
-import './App.css';
-import Button from "./components/Button"
+import React, { useState } from "react";
+import "./App.css";
+import Button from "./components/Button";
 function App() {
-  const [sound, setSound] = useState('')
+  const [sound, setSound] = useState("");
   //what happens when you click a button or press key
-  function playBeat()  {
-    setSound('beat')
-    console.log(sound)
+  function playBeat(beat) {
+    setSound(beat);
+    console.log(sound);
   }
   //Updates state to the correct button
 
@@ -17,7 +17,35 @@ function App() {
   return (
     <div className="App" id="drum-machine">
       <h1 id="display">{sound}</h1>
-    <Button clickHandler={playBeat}>cool beans</Button>
+      <div id="buttons">
+        <Button hash="button-Q" clickHandler={() => playBeat("Q")}>
+          Q
+        </Button>
+        <Button hash="button-W" clickHandler={() => playBeat("W")}>
+          W
+        </Button>
+        <Button hash="button-E" clickHandler={() => playBeat("E")}>
+          E
+        </Button>
+        <Button hash="button-A" clickHandler={() => playBeat("A")}>
+          A
+        </Button>
+        <Button hash="button-S" clickHandler={() => playBeat("S")}>
+          S
+        </Button>
+        <Button hash="button-D" clickHandler={() => playBeat("D")}>
+          D
+        </Button>
+        <Button hash="button-Z" clickHandler={() => playBeat("Z")}>
+          Z
+        </Button>
+        <Button hash="button-X" clickHandler={() => playBeat("X")}>
+          X
+        </Button>
+        <Button hash="button-C" clickHandler={() => playBeat("C")}>
+          C
+        </Button>
+      </div>
     </div>
   );
 }
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
-const Button = ({children, clickHandler}) => (
-    <button className="drum-pad" onClick={clickHandler}>
+const Button = ({children, clickHandler, hash}) => (
+    <button className="drum-pad" onClick={clickHandler} id={hash}>
     {children}
     </button>
 )
