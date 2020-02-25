4bbe9541515dd394c0773d3bceb5a4da53ebe9b6
uncannyvalle <jvalle0926@gmail.com>
2020-02-25 10:04:03 -0800

    made sound play appropriately

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React, { useState } from "react";
 import "./App.css";
 import Button from "./components/Button";
+
+//Songs
+import monkeySong from "./music/Monkey-Song.mp3";
+import marioSong from "./music/Mario-Paint.mp3";
+import flyswatter from "./music/Flyswatter.mp3"
+
 function App() {
   const [sound, setSound] = useState("");
   //what happens when you click a button or press key
   function playBeat(beat) {
+    let song = document.getElementById("song-" + beat);
+    song.pause();
     setSound(beat);
-    console.log(sound);
+    song.play();
   }
   //Updates state to the correct button
 
@@ -19,31 +27,31 @@ function App() {
       <h1 id="display">{sound}</h1>
       <div id="buttons">
         <Button hash="button-Q" clickHandler={() => playBeat("Q")}>
-          Q
+          Q<audio id="song-Q" src={monkeySong} autoPlay></audio>
         </Button>
         <Button hash="button-W" clickHandler={() => playBeat("W")}>
-          W
+          W<audio id="song-W" src={marioSong} autoPlay></audio>
         </Button>
         <Button hash="button-E" clickHandler={() => playBeat("E")}>
-          E
+          E<audio id="song-E" src={flyswatter} autoPlay></audio>
         </Button>
         <Button hash="button-A" clickHandler={() => playBeat("A")}>
-          A
+          A<audio id="song-A" src={marioSong} autoPlay></audio>
         </Button>
         <Button hash="button-S" clickHandler={() => playBeat("S")}>
-          S
+          S<audio id="song-S" src={marioSong} autoPlay></audio>
         </Button>
         <Button hash="button-D" clickHandler={() => playBeat("D")}>
-          D
+          D<audio id="song-D" src={marioSong} autoPlay></audio>
         </Button>
         <Button hash="button-Z" clickHandler={() => playBeat("Z")}>
-          Z
+          Z<audio id="song-Z" src={marioSong} autoPlay></audio>
         </Button>
         <Button hash="button-X" clickHandler={() => playBeat("X")}>
-          X
+          X<audio id="song-X" src={marioSong} autoPlay></audio>
         </Button>
         <Button hash="button-C" clickHandler={() => playBeat("C")}>
-          C
+          C<audio id="song-C" src={marioSong} autoPlay></audio>
         </Button>
       </div>
     </div>
